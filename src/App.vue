d697b6b4954f0ec8387eec3689ec53f17323456f
4DESIGNING <NAJI1HAYAT@yahoo.fr>
2020-02-13 12:07:08 +0300

    commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+ <div id="app">
+   <div class="wrapper">
+     <div class="flex-container">
+       <div class="box one"></div>
+       <div class="box two"></div>
+       <div class="box three"></div>
+      </div>
+      <div></div>
+   </div>
+ </div>
+</template>
+
+<script>
+</script>
+
+<style>
+@import url("style.css");
+
+</style>
